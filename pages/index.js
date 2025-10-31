eddda5b616256f7cea2c24bf8bec276a1afef349
Jonas <67794541+jojsj@users.noreply.github.com>
2025-10-31 23:10:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <p><h1>Oi Prinha, Eu te (x²+y²-1)³ = x²y³ !!!</h1><br/>se vc também, olha pra trás e dá um risinho</p>;
+  return (
+    <p>
+      <h1>Oi Prinha, Eu te (x²+y²-1)³ = x²y³ !!!</h1>
+      <br />
+      se vc também, olha pra trás e dá um risinho
+    </p>
+  );
 }
 
 export default Home;
